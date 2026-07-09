66ff8644be74796161ddb5b2c84e4ed276b46638
ajmalnisarnjkm-rgb <ajmalnisarnjkm@gmail.com>
2026-07-09 12:51:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ menuBtn?.addEventListener('click', () => navMenu.classList.toggle('active'));
 document.querySelectorAll('.nav a').forEach(link=>link.addEventListener('click',()=>navMenu.classList.remove('active')));
 const observer = new IntersectionObserver(entries=>{entries.forEach(entry=>{if(entry.isIntersecting){entry.target.classList.add('show');}})},{threshold:.15});
 document.querySelectorAll('.reveal').forEach(el=>observer.observe(el));
+function openVideo(link){
+  document.getElementById("videoFrame").src = link;
+  document.getElementById("videoModal").style.display = "flex";
+}
+
+function closeVideo(){
+  document.getElementById("videoModal").style.display = "none";
+  document.getElementById("videoFrame").src = "";
+}
